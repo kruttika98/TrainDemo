c9677efcaa30167368b1eff0dd44e1fc0c538685
trial <train@TRN0446.dmztrn.amdocs.trn>
2020-08-28 15:00:50 +0530

    first commit to local repository

---
+++ Area.java
@@ -0,0 +1,36 @@
+
+public class Area {
+	int length;
+	int width;
+	int side;
+	double radius;
+	
+	static double pi;
+	static {
+		pi=3.14;
+	}
+	
+	 Area(int length,int width)
+	 {
+		int rectangle=length*width;
+		 System.out.println("Area of Rectangle is "+rectangle);
+	 }
+	 Area(int side)
+	 {
+		int square=side*side;
+		 System.out.println("Area of Square is "+square);
+	 }
+	 Area(double radius)
+	 {
+		 double area=2*pi*radius;
+		 System.out.println("Area of Circle is "+area);
+	 }
+	public static void main(String args[]) {
+		Area a1=new Area(11,12);
+		Area a2=new Area(10);
+		Area a3=new Area(2.5);
+		
+	
+}
+
+}
